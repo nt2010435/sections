c92e54348c7ed815ad73079c9caac1dc072ed3a9
Nicholas Tran <2010435@crpwit1467smini.laclubs.com>
2017-01-24 06:33:13 -0800

    Added codes

--- Sections/ViewController.swift
+++ Sections/ViewController.swift
@@ -2,17 +2,47 @@
 //  ViewController.swift
 //  Sections
 //
-//  Created by Nicholas Tran on 1/24/17.
-//  Copyright © 2017 NT Dev. All rights reserved.
+//  Created by Kim Topley on 9/12/15.
+//  Copyright © 2015 Apress Inc. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    let sectionsTableIdentifier = "SectionsTableIndentifier"
+    var names: [String: [String]]!
+    var keys: [String]!
+    @IBOutlet weak var tableView: UITableView!
+    var searchController: UISearchController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        tableView.register(UITableViewCell.self,
+            forCellReuseIdentifier: sectionsTableIdentifier)
+        
+        let path = Bundle.main.path(
+                            forResource: "sortednames", ofType: "plist")
+        let namesDict = NSDictionary(contentsOfFile: path!)
+        names = namesDict as! [String: [String]]
+        keys = (namesDict!.allKeys as! [String]).sorted()
+        
+        let resultsController = SearchResultsController()
+        resultsController.names = names
+        resultsController.keys = keys
+        searchController =
+            UISearchController(searchResultsController: resultsController)
+        
+        let searchBar = searchController.searchBar
+        searchBar.scopeButtonTitles = ["All", "Short", "Long"]
+        searchBar.placeholder = "Enter a search term"
+        searchBar.sizeToFit()
+        tableView.tableHeaderView = searchBar
+        searchController.searchResultsUpdater = resultsController
+        
+        tableView.sectionIndexBackgroundColor = UIColor.black
+        tableView.sectionIndexTrackingBackgroundColor = UIColor.darkGray
+        tableView.sectionIndexColor = UIColor.white
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +50,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: Table View Data Source Methods
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return keys.count
+    }
+    
+    func tableView(_ tableView: UITableView,
+                numberOfRowsInSection section: Int) -> Int {
+        let key = keys[section]
+        let nameSection = names[key]!
+        return nameSection.count
+    }
+    
+    func tableView(_ tableView: UITableView,
+                titleForHeaderInSection section: Int) -> String? {
+        return keys[section]
+    }
+    
+    func tableView(_ tableView: UITableView,
+                cellForRowAt indexPath: IndexPath)
+                    -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: sectionsTableIdentifier, for: indexPath)
+        as UITableViewCell
+        
+        let key = keys[indexPath.section]
+        let nameSection = names[key]!
+        cell.textLabel?.text = nameSection[indexPath.row]
+        
+        return cell
+    }
+    
+    func sectionIndexTitles(for tableView: UITableView)
+                    -> [String]? {
+        return keys
+    }
 }
 
